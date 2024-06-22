06e5e95c06c4d2d9c903ede1d1ddd2070595d196
elmin412 <115942958+elmin412@users.noreply.github.com>
2024-06-22 09:42:31 +0200

    fix

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import express from 'express';
 import pino from "pino-http"
 import env from "./utils/env.js";
 import { getContacts, getContactById } from "./services/contact-service.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 
 
 const port = Number(env("PORT", 3000));
@@ -27,44 +29,38 @@ app.get('/contacts', async(req, res) => {
     const data  = await getContacts();
     res.json({
       status: 200,
-      data ,
-      message: "Successfully found contacts!"
+      message: "Successfully found contacts!",
+      data 
+      
     });
   });
 
 // получения одного контакта по id
   app.get("/contacts/:contactId", async(req, res) => {
-    const {contactId} = req.params;
   try {
-    
+    const {contactId} = req.params;
     const data = await getContactById(contactId);
-    
-
     if(!data) {
       return res.status(404).json({
         message: `Сontact with id=${contactId} not found!`
       });
     } 
       res.json({
-      data,
       status: 200,
-      message: `Successfully found contact with id=${contactId}!`
+      message: `Successfully found contact with id=${contactId}!`,
+      data
     });
   } catch (error) {
     res.status(500).json({
-
-        message: 'Something wrong',
+        message: ' Wrong',
         
       })
   }}
 );
 
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-        message: 'Not found',
-    });
-  });
-
+  app.use('*', notFoundHandler);
+  
+  app.use(errorHandler);
 
   app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
